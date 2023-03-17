3cbeea65b59e6eca8b842cc16f8954d6b65d0e09
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-03-16 23:07:33 -0300

    Se agregaron las casillas y se cambio la logica del script del tablero.

--- Scripts/Tablero.gd
+++ Scripts/Tablero.gd
@@ -1,25 +1,42 @@
 extends Control
 
-#Sprites path
-@export_subgroup("Property Textures")
-@export_global_file("*.png") var sprite_go
-@export_global_file("*.png") var sprite_brown
-@export_global_file("*.png") var sprite_blue_light
-@export_global_file("*.png") var sprite_purple
-@export_global_file("*.png") var sprite_orange
-@export_global_file("*.png") var sprite_red
-@export_global_file("*.png") var sprite_yellow
-@export_global_file("*.png") var sprite_green
-@export_global_file("*.png") var sprite_blue
-
-
+var board = {
+	properties = [
+		{
+			id = 1,
+			prop_name = "Avenida Mediterranea",
+			price = 60,
+			pos = {
+				x = 1305,
+				y = 1470
+			}
+		},
+		{
+			id = 3,
+			prop_name = "Avenida Baltica",
+			price = 60,
+			pos = {
+				x = 1045,
+				y = 1470
+			}
+		},
+		{
+			id = 6,
+			prop_name = "Avenida Oriental",
+			price = 100,
+			pos = {
+				x = 1305,
+				y = 1470
+			}
+		}
+	]
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var newSprite = Sprite2D.new()
-	newSprite.texture = load(sprite_go)
-	add_child(newSprite)
-
+	$c_brown_1.setProperty(1, "Avenida Mediterranea", 60)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
--- Scripts/property.gd
+++ Scripts/property.gd
@@ -2,9 +2,6 @@ extends Sprite2D
 
 var property = { _id=0, name="some name", price=1111 }
 
-func _ready():
-	updateProperty()
-
 func setProperty(propId, propName, propPrice):
 	property._id = propId
 	property.name = propName
